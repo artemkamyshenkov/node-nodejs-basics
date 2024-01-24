adfcb836343a556051b01158519c41426cb97131
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-24 12:44:06 +0300

    add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { promises as fsPromises } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const newFilePath = path.join(dirname, 'files', 'fresh.txt');
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  fsPromises
+    .access(newFilePath, fs.constants.F_OK)
+    .then(() => {
+      console.error('FS operation failed');
+    })
+    .catch((err) => {
+      fsPromises.writeFile(newFilePath, fileContent);
+    });
 };
 
 await create();
