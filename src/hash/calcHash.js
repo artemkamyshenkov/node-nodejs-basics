955e9a612629b5b45740447c858a56a0c8d68fb6
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-25 19:40:43 +0300

    add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  stream.on('end', () => {
+    const hashRes = hash.digest('hex');
+    console.log(`SHA256: ${hashRes}`);
+  });
+
+  stream.on('error', (e) => {
+    console.log(e);
+  });
 };
 
 await calculateHash();
