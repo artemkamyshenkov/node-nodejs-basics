e067ee6a9d99c6b915d002b741a938c8cee02a72
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-25 18:44:31 +0300

    add list js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { promises as fsPromises, readdirSync, constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const folderPath = path.join(dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.access(folderPath, constants.F_OK);
+    const files = await fsPromises.readdir(folderPath);
+    files.forEach((file) => console.log(file));
+  } catch (error) {
+    console.log('FS operation failed');
+  }
 };
 
 await list();
