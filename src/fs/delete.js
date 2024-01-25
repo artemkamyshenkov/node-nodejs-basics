7d955581cdfe3fd48a747e025eb4196566749cd3
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-25 18:40:52 +0300

    add delete js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { promises as fsPromises, constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.access(filePath, constants.F_OK);
+    await fsPromises.unlink(filePath);
+  } catch (error) {
+    console.log('FS operation failed');
+  }
 };
 
 await remove();
