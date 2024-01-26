7a62d9548f8008c61456876e6e06bf2af27786ae
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-26 17:53:24 +0300

    add write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,33 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(filePath);
+  process.stdin.setEncoding('utf-8');
+
+  process.stdout.write('print here: ');
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if (chunk) {
+      writeStream.write(chunk);
+    }
+  });
+
+  process.stdin.on('end', () => {
+    writeStream.end();
+  });
+
+  process.stdin.on('error', (e) => {
+    console.log('Error!');
+  });
+
+  writeStream.on('error', (e) => {
+    console.log('Error!');
+  });
 };
 
 await write();
