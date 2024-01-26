e376de867d8497caa99fb9473bacae0c6a71bf97
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-26 17:37:44 +0300

    add streams read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on('error', () => {
+    console.log('Error reading the file');
+  });
 };
 
 await read();
