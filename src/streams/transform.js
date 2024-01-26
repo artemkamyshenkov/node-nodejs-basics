e032436bbdb6b356ddabdc12dadd3d2d65f4f41d
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-26 18:21:26 +0300

    add transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,32 @@
+import { Transform } from 'stream';
+
+const createTransform = () =>
+  new Transform({
+    transform(chunk, encoding, callback) {
+      const reverse = chunk.toString().split('').reverse().join('');
+      this.push(reverse);
+      callback();
+    },
+  });
+
 const transform = async () => {
-    // Write your code here 
+  process.stdin.setEncoding('utf-8');
+  const reverseTransform = createTransform();
+  const readStream = process.stdin;
+  const writeStream = process.stdout;
+
+  readStream.pipe(reverseTransform).pipe(writeStream);
+
+  writeStream.write('print here: ');
+
+  readStream.on('error', (e) => {
+    console.log('Error!');
+  });
+
+  readStream.once('data', () => {
+    readStream.destroy();
+    writeStream.destroy();
+  });
 };
 
 await transform();
