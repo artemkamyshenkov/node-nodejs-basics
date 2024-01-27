cb87ffadbd255df72573a990c980b4bb20392d5d
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-27 15:53:49 +0300

    add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { spawn, fork } from 'child_process';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const workerFile = path.join(dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(workerFile, args, {
+    stdio: 'pipe',
+  });
+
+  childProcess.stdout.on('data', (data) => {
+    const output = data.toString();
+    process.stdout.write(`Child process: ${output}`);
+  });
+
+  process.stdin.on('data', (data) => {
+    childProcess.stdin.write(data);
+  });
+
+  childProcess.on('close', (code) => {
+    console.log(code);
+    process.exit();
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['1', '2', '3']);
