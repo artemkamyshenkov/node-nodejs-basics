e61c516430d22b9f222814bd56e972fc08b7f1df
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-01-27 14:24:55 +0300

    add workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import os from 'os';
+import { Worker } from 'worker_threads';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const workerFile = path.join(dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+  const result = [];
+  const workersCount = os.cpus()?.length || 1;
+
+  const createWorkerPromise = (worker, idx) =>
+    new Promise((resolve, reject) => {
+      worker.on('message', (data) => {
+        const { type, data: res } = data;
+        if (type === 'result') {
+          result[idx] = { status: 'resolved', data: res };
+          resolve();
+        }
+      });
+
+      worker.on('error', (e) => {
+        result[idx] = { status: 'error', data: null };
+        reject(e);
+      });
+    });
+
+  for (let i = 0; i < workersCount; i++) {
+    const worker = new Worker(workerFile);
+
+    const dataSend = 10 + i;
+
+    worker.postMessage({ type: 'calculate', data: dataSend });
+
+    await createWorkerPromise(worker, i);
+  }
+
+  console.log(result);
+  return result;
 };
 
 await performCalculations();
